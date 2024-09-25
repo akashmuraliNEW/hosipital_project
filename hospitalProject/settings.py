ab47573df4473bf628f2ce00f1e846c87d8cd96d
AKASH KM <akashunni869@gmail.com>
2024-09-25 14:08:57 +0530

    sum confirguration for koyeb

--- hospitalProject/settings.py
+++ hospitalProject/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = 'django-insecure-p4w!%xvvf($!5oq41s6#!k^s$eznqr-8vjkwu6bygo$)z@c#iv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'indirect-katherine-mywebapps-46b1d7d4.koyeb.app',
+    'localhost',
+    '127.0.0.1',
+]
+
+
 
 
 # Application definition
@@ -47,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,11 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
-
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
